62d1e96a5a91c3c9f80423a2ca6d1c91bb6f9753
Sean Levin <slevin@yahooinc.com>
2022-08-11 22:34:18 +0100

    update perf tests to work with newer api

--- examples/perftests/src/index.ts
+++ examples/perftests/src/index.ts
@@ -7,7 +7,7 @@ class RootExtent extends bg.Extent {
     addExtents: bg.Moment;
     extentAdder : bg.Moment;
     linkUpdater : bg.Moment;
-    bulkResources : bg.Moment[][] = [];
+    bulkResources : bg.Moment<undefined>[][] = [];
     bulkBehaviors : bg.Behavior[][] = [];
     subextents : Sub1Extent[] = [];
     width: number = 300;
@@ -15,47 +15,47 @@ class RootExtent extends bg.Extent {
 
     constructor(g: bg.Graph) {
         super(g);
-        this.root = new bg.Moment<undefined>(this);
-        this.extentAdder = new bg.Moment<undefined>(this);
-        this.linkUpdater = new bg.Moment<undefined>(this);
-        this.addExtents = new bg.Moment<undefined>(this);
+        this.root = this.moment();
+        this.extentAdder = this.moment();
+        this.linkUpdater = this.moment();
+        this.addExtents = this.moment();
 
-        this.makeBehavior([this.addExtents], [this.extentAdder], (extent) => {
-            for (let i = 0; i<3; i++) {
-                let e = new Sub1Extent(g, this.root);
-                e.addToGraph();
-                this.subextents.push(e);
-            }
-            this.extentAdder.update();
-        });
-
-        this.makeBehavior([this.extentAdder], [this.linkUpdater], (extent) => {
-            // make all the behaviors in the first row of this extent
-            // demand the resources in the first row of each subextent
-            for (let j = 0; j < this.width; j++) {
-                let newDemands : bg.Resource[] = [];
-                for (let demand of this.bulkBehaviors[0][j].demands ?? []) {
-                    newDemands.push(demand);
+        this.behavior()
+            .supplies(this.extentAdder)
+            .demands(this.addExtents)
+            .runs(ext => {
+                for (let i = 0; i<3; i++) {
+                    let e = new Sub1Extent(g, this.root);
+                    e.addToGraph();
+                    this.subextents.push(e);
                 }
+                this.extentAdder.update();
+
+            });
+
+        this.behavior()
+            .supplies(this.linkUpdater)
+            .demands(this.extentAdder)
+            .runs(extent => {
+                // make all the behaviors in the first row of this extent
+                // demand the resources in the first row of each subextent
+                let newDemands: bg.Moment[] = [];
                 for (let ext of this.subextents) {
                     newDemands.push(...ext.bulkResources[0])
                 }
-                this.bulkBehaviors[0][j].setDemands(newDemands)
-            }
-
-            // make each behavior in the last row of each subextent
-            // demand each resource in the last row of this extent
-            for (let ext of this.subextents) {
-                for (let j = 0; j < ext.width; j++) {
-                    let newDemands : bg.Resource[] = [];
-                    for (let demand of ext.bulkBehaviors[ext.depth - 1][j].demands ?? []) {
-                        newDemands.push(demand);
-                    }
-                    newDemands.push(...this.bulkResources[this.depth - 1]);
-                    ext.bulkBehaviors[ext.depth - 1][j].setDemands(newDemands);
+                for (let j = 0; j < this.width; j++) {
+                    this.bulkBehaviors[0][j].setDynamicDemands(newDemands)
                 }
-            }
-        });
+
+                // make each behavior in the last row of each subextent
+                // demand each resource in the last row of this extent
+                let allSupplied: bg.Moment[] = this.bulkResources[this.depth - 1];
+                for (let ext of this.subextents) {
+                    for (let j = 0; j < ext.width; j++) {
+                        ext.bulkBehaviors[ext.depth - 1][j].setDynamicDemands(allSupplied);
+                    }
+                }
+            });
 
         let previousRow: bg.Moment[] = [];
         for (let i = 0; i < this.depth; i++) {
@@ -63,11 +63,14 @@ class RootExtent extends bg.Extent {
             this.bulkBehaviors[i] = [];
             let demands = [this.root, this.linkUpdater].concat(previousRow);
             for (let j = 0; j < this.width; j++) {
-                let moment = new bg.Moment(this);
+                let moment: bg.Moment<undefined> = this.moment();
                 this.bulkResources[i][j] = moment;
-                this.bulkBehaviors[i][j] = this.makeBehavior(demands, [moment], (extent) => {
-                    moment.update();
-                });
+                this.bulkBehaviors[i][j] = this.behavior()
+                    .supplies(moment)
+                    .demands(...demands)
+                    .runs(ext => {
+                        moment.update();
+                    });
             }
             previousRow = this.bulkResources[i];
         }
@@ -89,11 +92,14 @@ class Sub1Extent extends bg.Extent {
             this.bulkBehaviors[i] = [];
             let demands = previousRow;
             for (let j = 0; j < this.width; j++) {
-                let moment = new bg.Moment(this);
+                let moment: bg.Moment<undefined> = this.moment();
                 this.bulkResources[i][j] = moment;
-                this.bulkBehaviors[i][j] = this.makeBehavior(demands, [moment], (extent) => {
-                    moment.update();
-                });
+                this.bulkBehaviors[i][j] = this.behavior()
+                    .supplies(moment)
+                    .demands(...demands)
+                    .runs(ext => {
+                        moment.update();
+                    });
             }
             previousRow = this.bulkResources[i];
         }
@@ -109,34 +115,30 @@ obs.observe({ type: 'measure' });
 console.log('starting...');
 // adding test
 
-for (let i = 0; i < 1.; i++) {
+for (let i = 0; i < 1; i++) {
     let g = new bg.Graph()
     let e = new RootExtent(g)
 
     performance.mark('1')
-    g.action(null, () => {
-        e.addToGraph();
-    });
+    e.addToGraphWithAction();
     performance.measure('add root', '1');
     performance.mark('2')
-    g.action(null, () => {
-        e.root.update();
-    })
+    e.root.updateWithAction();
     performance.measure('simple update', '2');
 
     performance.mark('3')
-    g.action(null, () => {
+    g.action(() => {
         e.root.update();
         e.addExtents.update();
     });
     performance.measure('add subextents', '3')
 
-
     performance.mark('4')
-    g.action(null, () => {
+    g.action(() => {
         for (let ext of e.subextents) {
             ext.removeFromGraph();
         }
+        e.removeFromGraph();
     });
     performance.measure('remove', '4');
 }
