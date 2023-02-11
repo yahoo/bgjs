7a2561f37a7b3374015c6b0dbeab302324637642
Sean Levin <slevin@yahooinc.com>
2023-02-10 22:52:22 -0800

    update browser example project to newer bg version
    (cherry picked from commit 21d3809f2100c58afa2dc8e5ec9cef52df180d56)

--- examples/browser/main.js
+++ examples/browser/main.js
@@ -35,94 +35,96 @@ function updateLoginStatus(status) {
 }
 
 class LoginExtent extends bg.Extent {
-    email = new bg.State(this,null);
-    password = new bg.State(this,null);
-    emailValid = new bg.State(this,false);
-    passwordValid = new bg.State(this,false);
-    loginEnabled = new bg.State(this,false);
-    loggingIn = new bg.State(this, false);
-    loginClick = new bg.Moment(this);
-    loginComplete = new bg.Moment(this);
+    email = this.state(null);
+    password = this.state(null);
+    emailValid = this.state(false);
+    passwordValid = this.state(false);
+    loginEnabled = this.state(false);
+    loggingIn = this.state(false);
+    loginClick = this.moment();
+    loginComplete = this.moment(this);
 
     constructor(graph) {
         super(graph);
 
-        this.makeBehavior([this.email, this.addedToGraph], [this.emailValid], (extent) => {
-
-            let email = extent.email.value;
-            let emailValid = validateEmail(email);
-            extent.emailValid.update(emailValid);
-            extent.sideEffect((extent) => {
-                inputFeedback("emailFeedback", extent.emailValid.value);
-            }, undefined, null);
-
-        });
-
-
-        this.makeBehavior([this.password, this.addedToGraph], [this.passwordValid], (extent) => {
-
-            let password = extent.password.value ?? "";
-            let passwordValid = password.length > 0;
-            extent.passwordValid.update(passwordValid);
-            extent.sideEffect((extent) => {
-                inputFeedback("passwordFeedback", extent.passwordValid.value);
-            }, undefined, "passwordFeedback");
-
-        });
-
-
-        this.makeBehavior([this.emailValid, this.passwordValid, this.loggingIn, this.addedToGraph], [this.loginEnabled], (extent) => {
-
-            let enabled = extent.emailValid.value && extent.passwordValid.value && !extent.loggingIn.value;
-            extent.loginEnabled.update(enabled)
-            extent.sideEffect((extent) => {
-                loginButtonEnable(extent.loginEnabled.value);
-            }, undefined, "enable login button");
-
-        });
-
-
-        this.makeBehavior([this.loginClick, this.loginComplete, this.addedToGraph], [this.loggingIn], (extent) => {
-
-            if (extent.loginClick.justUpdated && extent.loginEnabled.traceValue) {
-                extent.loggingIn.update(true);
-            } else if (extent.loginComplete.justUpdated && extent.loggingIn.value) {
-                extent.loggingIn.update(false);
-            }
-
-            if (extent.loggingIn.justUpdatedTo(true)) {
+        this.behavior()
+            .supplies(this.emailValid)
+            .demands(this.email, this.addedToGraph)
+            .runs(extent => {
+                let email = extent.email.value;
+                let emailValid = validateEmail(email);
+                extent.emailValid.update(emailValid);
                 extent.sideEffect((extent) => {
-                    loginToServer(extent.email.value, extent.password.value);
-                }, undefined, "login api call");
-            }
+                    inputFeedback("emailFeedback", extent.emailValid.value);
+                }, undefined, null);
+            });
 
-        });
+        this.behavior()
+            .supplies(this.passwordValid)
+            .demands(this.password, this.addedToGraph)
+            .runs(extent => {
+                let password = extent.password.value ?? "";
+                let passwordValid = password.length > 0;
+                extent.passwordValid.update(passwordValid);
+                extent.sideEffect((extent) => {
+                    inputFeedback("passwordFeedback", extent.passwordValid.value);
+                }, undefined, "passwordFeedback");
+            });
 
+        this.behavior()
+            .supplies(this.loginEnabled)
+            .demands(this.emailValid, this.passwordValid, this.loggingIn, this.addedToGraph)
+            .runs(extent => {
 
-        this.makeBehavior([this.loggingIn, this.loginComplete, this.addedToGraph], null, (extent) => {
-            extent.sideEffect((extent) => {
-                let status = "&nbsp;"
-                if (extent.loggingIn.value) {
-                    status = "Logging in...";
-                } else if (extent.loggingIn.justUpdatedTo(false)) {
-                    if (extent.loginComplete.justUpdated && extent.loginComplete.value) {
-                        status = "Login Success";
-                    } else if (extent.loginComplete.justUpdated && !extent.loginComplete.value) {
-                        status = "Login Failed";
-                    }
+                let enabled = extent.emailValid.value && extent.passwordValid.value && !extent.loggingIn.value;
+                extent.loginEnabled.update(enabled)
+                extent.sideEffect((extent) => {
+                    loginButtonEnable(extent.loginEnabled.value);
+                }, undefined, "enable login button");
+
+            });
+
+        this.behavior()
+            .supplies(this.loggingIn)
+            .demands(this.loginClick, this.loginComplete, this.addedToGraph)
+            .runs(extent => {
+
+                if (extent.loginClick.justUpdated && extent.loginEnabled.traceValue) {
+                    extent.loggingIn.update(true);
+                } else if (extent.loginComplete.justUpdated && extent.loggingIn.value) {
+                    extent.loggingIn.update(false);
                 }
-                updateLoginStatus(status);
-            }, undefined, "login status");
-        });
 
+                if (extent.loggingIn.justUpdatedTo(true)) {
+                    extent.sideEffect((extent) => {
+                        loginToServer(extent.email.value, extent.password.value);
+                    }, undefined, "login api call");
+                }
+            });
+
+        this.behavior()
+            .demands(this.loggingIn, this.loginComplete, this.addedToGraph)
+            .runs(extent => {
+                extent.sideEffect((extent) => {
+                    let status = "&nbsp;"
+                    if (extent.loggingIn.value) {
+                        status = "Logging in...";
+                    } else if (extent.loggingIn.justUpdatedTo(false)) {
+                        if (extent.loginComplete.justUpdated && extent.loginComplete.value) {
+                            status = "Login Success";
+                        } else if (extent.loginComplete.justUpdated && !extent.loginComplete.value) {
+                            status = "Login Failed";
+                        }
+                    }
+                    updateLoginStatus(status);
+                }, undefined, "login status");
+            });
     }
 }
 
 let graph = new bg.Graph();
 let loginExtent = new LoginExtent(graph);
-graph.action("startup", () => {
-    loginExtent.addToGraph()
-});
+loginExtent.addToGraphWithAction();
 
 document.getElementById("email").addEventListener("input", (event) => {
     loginExtent.email.updateWithAction(event.target.value);
